9a617919ad10e433e3577b4c789f6d40afe706e5
irfan <pakinpaz@gmail.com>
2024-03-27 14:52:13 +0300

    paketler güncellendi ve mongoDb bağlanıldı

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.set('strictQuery', true);
+
+const port= process.env.PORT || 5000;
+const start = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_URL);
+        console.log('Connected to MongoDB');
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+ 
+start();
+
+
